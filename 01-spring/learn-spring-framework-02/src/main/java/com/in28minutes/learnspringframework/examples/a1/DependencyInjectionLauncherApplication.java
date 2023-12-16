ae10702ec88612f3c95e036a2a9b221a4a8e82c1
Sriram.bit.bot <sriramsaravanakumar@gmail.com>
2023-12-17 00:48:10 +0530

    Setter Injection - Autowiring using setters

--- 01-spring/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/a1/DependencyInjectionLauncherApplication.java
+++ 01-spring/learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/a1/DependencyInjectionLauncherApplication.java
@@ -1,7 +1,7 @@
 package com.in28minutes.learnspringframework.examples.a1;
 
 
-// Field Annotation
+// Setter Injection
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
@@ -16,10 +16,26 @@ import java.util.Arrays;
 
 @Component
 class YourBusinessClass{
-    @Autowired // Using Autowired annotation to autowire, remove this and you will print null
-    Dependency1 dependency1; // Injecting these fields
-    @Autowired
+    Dependency1 dependency1;
     Dependency2 dependency2;
+
+    public Dependency1 getDependency1() {
+        return dependency1;
+    }
+    public Dependency2 getDependency2() {
+        return dependency2;
+    }
+
+    @Autowired
+    public void setDependency2(Dependency2 dependency2) {
+        System.out.println("Setter Injection - setDependency2");
+        this.dependency2 = dependency2;
+    }
+    @Autowired
+    public void setDependency1(Dependency1 dependency1) {
+        System.out.println("Setter Injection - setDependency1");
+        this.dependency1 = dependency1;
+    }
 
     public String toString(){
         return "Using " + dependency1 +" and "+dependency2;
